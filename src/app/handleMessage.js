03f18b6c9b28c8bdbd52260f093a443f862ee2ff
meaghanravagnati <mravagnati@frba.utn.edu.ar>
2026-01-17 21:13:47 -0300

    add: array con mensajes

---
+++ src/app/handleMessage.js
@@ -0,0 +1,19 @@
+const messages = [{
+    id: 1,
+    text: "Hola! ¿Querés venir por un cafe?",
+    hour: "14:12",
+    me: false
+},
+{
+    id: 2,
+    text: "Si, perfecto. ¿A las 18 te viene bien?",
+    hour: "14:13",
+    me: true
+},
+{
+    id: 3,
+    text: "Dale, en la plaza entonces 😎😃",
+    hour: "14:14",
+    me: false
+}
+]
